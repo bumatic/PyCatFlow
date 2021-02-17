c7a23030556521204a07fe918eb86fa362e0f50a
herbertmn <71848508+herbertmn@users.noreply.github.com>
2021-02-17 20:45:30 +0100

    Add files via upload

---
+++ nodes.py
@@ -0,0 +1,51 @@
+class Node:
+    def __init__(self,index,x,y,size,value,width,label,subtag):
+        self.x=x
+        self.index=index
+        self.y=y
+        self.size=size
+        self.value=value
+        self.width=width
+        self.label=label
+        self.subtag=subtag
+
+def nodify(data,size,maxValue=10,sort_by="frequency"):
+    d={}
+    if sort_by=="frequency":
+        for x in data.items():
+            d[x[0]]={k: v for k, v in sorted(x[1].items(), key=lambda item: item[1],reverse=True)}
+    else:
+        d=data
+
+    labels=[list(x[1].keys()) for x in d.items()]
+    values=[[y[0] if type(y)==tuple else y for y in x[1].values()] for x in d.items()]
+    m=max([max([y[0] if type(y)==tuple else y for y in x[1].values()]) for x in d.items()] )
+    subtags=[[y[1] if type(y)==tuple else "null" for y in x[1].values()] for x in d.items()]
+    headers=list(d.keys())
+    node_x=0
+    count=0
+    count2=0
+
+    nodes=[]
+    sequence={}
+
+    for l,v,s in zip (labels,values,subtags):
+        if count2<len(labels)-1:
+            count2+=1  
+        node_y=0
+        for x,y,z in zip (l,v,s):
+            #node_y+=y
+            nodes.append(Node(count,node_x,node_y,y*maxValue/m,y,size,x,z))
+
+            count+=1
+           
+        node_x+=size
+
+    for n in nodes:
+        if n.label in sequence.keys():
+            sequence[n.label].append(n.index)
+        else:
+            sequence[n.label]=[]
+            sequence[n.label].append(n.index)
+    
+    return [headers,nodes,sequence]
---
+++ viz.py
@@ -0,0 +1,212 @@
+import drawSvg as draw
+from matplotlib import cm,colors
+import pycatflow as pcf
+
+def genSVG(nodes,spacing,width=None,heigth=None,color_startEnd=True,color_subtag=True,nodes_color="gray",start_node_color="green",end_node_color="red",palette=None,show_labels=True,label_text="tag",label_color="black",label_size=5,label_shortening="clip",label_position="nodes"):
+    headers=nodes[0]
+    
+    sequence=nodes[2]
+    
+    n_x_spacing=spacing
+    n_y_spacing=spacing+(spacing/5)
+    #h_x_shift=[n_x_spacing]
+    points=[]
+    for n in nodes[1]:
+        
+        if n.index>0 and n.x==nodes[1][n.index-1].x:
+            n_y_spacing+=spacing/5+n.size
+        else:
+            n_y_spacing=spacing+(spacing/5)+n.size
+        if n.index>0 and n.x!=nodes[1][n.index-1].x:
+            n_x_spacing+=spacing
+            #h_x_shift.append(n.x+n_x_spacing)
+        points.append(pcf.Node(n.index,n.x+ n_x_spacing,n.y+n_y_spacing,n.size,n.value,n.width,n.label,n.subtag))
+    
+    
+    if width is None and heigth is None:
+        
+        width=spacing*4+max([x.x for x in points])
+        heigth=spacing*4+max([x.y for x in points])
+        
+    elif heigth is None:
+        heigth=spacing*4+max([x.y for x in points])
+        points=[pcf.Node(n.index,(n.x*(width-spacing*2)/max([x.x for x in points])),n.y,n.size,n.value,n.width,n.label,n.subtag) for n in points]
+    elif width is None:
+        width=spacing*4+max([x.x for x in points])
+        points=[pcf.Node(n.index,n.x,(n.y*(heigth-spacing*2)/max([x.y for x in points])),n.size,n.value,n.width,n.label,n.subtag) for n in points]
+        n_y_spacing=spacing+(spacing/5)
+        for n in points:
+            if n.index>0 and n.x==nodes[1][n.index-1].x:
+                n_y_spacing+=spacing/5
+            else:
+                n_y_spacing=spacing+(spacing/5)
+            n.y+=n_y_spacing
+
+        
+    else:  
+        points=[pcf.Node(n.index,(n.x*(width-spacing)/max([x.x for x in points])),(n.y*(heigth-spacing)/max([x.y for x in points])),n.size,n.value,n.width,n.label,n.subtag) for n in points]      
+
+    
+    if palette is not None:
+        palette=cm.get_cmap(palette[0],palette[1]).colors
+        count=0
+        subtag_colors={}
+        for e in set([n.subtag for n in points]):
+            if count<len(palette):
+                count+=1
+            subtag_colors[e]=colors.to_hex(palette[count])
+    else:
+        palette=[[x/12,x/12,x/12,1] for x in range(4,12)]
+        count=0
+        subtag_colors={}
+        for e in set([n.subtag for n in points]):
+            if count<len(palette)-1:
+                count+=1
+            subtag_colors[e]=colors.to_hex(palette[count])
+
+
+
+    
+        
+    
+    d = draw.Drawing(width, heigth,displayInline=True)
+    r = draw.Rectangle(0,0,width,heigth, stroke_width=2, stroke='black',fill="white")
+    d.append(r)
+
+    #headers
+    h_x_shift=[points[0].x]
+    
+    for x in points:
+        if x.x!=points[x.index-1].x and x.index>0:
+            h_x_shift.append(x.x)
+    
+    for h,x in zip (headers,h_x_shift):
+        l=label_size
+        if label_shortening=="resize":
+            while len(h)*(l/2)>spacing:
+                if x!=max(h_x_shift):
+                    l-=1
+                else:
+                    break
+            d.append(draw.Text(h,x=x,y=heigth-spacing,fontSize=l,fill=label_color))
+        elif label_shortening=="clip":
+            clip = draw.ClipPath()
+            clip.append(draw.Rectangle(x,heigth-spacing,spacing,label_size))
+            d.append(draw.Text(h,x=x,y=heigth-spacing,fontSize=l,clip_path=clip,fill=label_color))
+        elif label_shortening=="new_line":
+            if len(h)*(label_size/2)>spacing:
+                txt=[h[:int(spacing/(label_size/2))],h[int(spacing/(label_size/2)):]] 
+                while len(txt)*l>(l+spacing/8):
+                    l-=1
+            else:
+                txt=h                                                              
+            d.append(draw.Text(txt,x=x,y=heigth-spacing,fontSize=l,fill=label_color))
+    
+    #lines
+    for n in sequence.items():        
+        if len(n[1])>1:            
+            for k in n[1][:-1]:
+                if color_subtag == True:
+                    color=subtag_colors[points[k].subtag]
+                else:
+                    color=nodes_color
+                p = draw.Path(fill=color)           
+                p.M(points[k].x+points[k].width,heigth-points[k].y)            
+                p.L(points[k].x+points[k].width,heigth-points[k].y+points[k].size)
+              
+            
+                if points[k].y==points[n[1][n[1].index(k)+1]].y:
+                    p.L(points[n[1][n[1].index(k)+1]].x,heigth-points[k].y+points[k].size)                                    
+                    p.L(points[n[1][n[1].index(k)+1]].x,heigth-points[k].y)
+                    
+                else:
+                    xMedium=((points[n[1][n[1].index(k)+1]].x-(points[k].x+points[k].width))/2)+(points[k].x+points[k].width)
+                    yMedium=(((heigth-points[k].y+points[k].size)-(heigth-points[n[1][n[1].index(k)+1]].y+points[k].size))/2)+(heigth-points[n[1][n[1].index(k)+1]].y+points[k].size)
+                    yMedium2=(((heigth-points[k].y)-(heigth-points[n[1][n[1].index(k)+1]].y))/2)+(heigth-points[n[1][n[1].index(k)+1]].y)
+                    p.Q(points[k].x+points[k].width+(spacing/2),heigth-points[k].y+points[k].size,xMedium,yMedium)                    
+                    #p.Q(points[k].x+points[k].width+(spacing/2),heigth-points[n[1][n[1].index(k)+1]].y+points[n[1][n[1].index(k)+1]].size,points[n[1][n[1].index(k)+1]].x,heigth-points[n[1][n[1].index(k)+1]].y+points[n[1][n[1].index(k)+1]].size)
+                    p.T(points[n[1][n[1].index(k)+1]].x,heigth-points[n[1][n[1].index(k)+1]].y+points[n[1][n[1].index(k)+1]].size)
+                    #p.L(points[n[1][n[1].index(k)+1]].x,heigth-points[n[1][n[1].index(k)+1]].y+points[n[1][n[1].index(k)+1]].size) 
+                    p.L(points[n[1][n[1].index(k)+1]].x,heigth-points[n[1][n[1].index(k)+1]].y)
+                    p.Q(points[n[1][n[1].index(k)+1]].x-(spacing/2),heigth-points[n[1][n[1].index(k)+1]].y,xMedium,yMedium2)
+                    #p.Q(points[k].x+points[k].width+(spacing/2),yMedium,points[k].x+points[k].width,points[k].y)
+                    p.T(points[k].x+points[k].width,heigth-points[k].y)  
+
+                                  
+                p.Z()
+                d.append(p)       
+                     
+
+    #nodes
+    #n_x_shift=spacing
+    #n_y_shift=spacing
+    for node in points:
+        if color_startEnd == True and color_subtag == True:
+            if node.label not in [n.label for n in points][:node.index]:
+                color=start_node_color
+            elif node.label not in [n.label for n in points][node.index+1:] and node.index<len(points):
+                color=end_node_color
+            else:
+                color=subtag_colors[node.subtag]
+        elif color_startEnd == True and color_subtag == False:
+            if node.label not in [n.label for n in points][:node.index]:
+                color=start_node_color
+            elif node.label not in [n.label for n in points][node.index+1:] and node.index<len(points):
+                color=end_node_color
+            else:
+                color=nodes_color
+        elif color_startEnd == False and color_subtag == True:
+            color=subtag_colors[node.subtag]
+        elif color_startEnd == False and color_subtag == False:
+            color=nodes_color
+        #if node.x!=points[node.index-1].x:
+            #n_x_shift+=spacing
+        r = draw.Rectangle(node.x,heigth-node.y,node.width,node.size, fill=color,stroke="black")
+        d.append(r)
+
+
+        if show_labels==True:
+            
+            if label_text=="tag_count":
+                txt=node.label+' ('+str(node.value)+')'
+            elif label_text=="tag":
+                txt=node.label
+            elif label_text=="tag_subtag":
+                txt=node.label+' ('+str(node.subtag)+')'  
+            
+            #if len(txt)*(label_size/2)>spacing-2*(spacing/8):
+                #txt=[txt[:int((spacing-2*(spacing/8))/(label_size/2))],txt[int((spacing-2*(spacing/8))/(label_size/2)):]]
+            l=label_size
+            if label_shortening=="resize":
+                while len(txt)*(l/2)>spacing-(spacing/8):
+                    if node.x!=max([n.x for n in points]):
+                        l-=1
+                    else:
+                        break
+            elif label_shortening=="clip":
+                clip = draw.ClipPath()
+                clip.append(draw.Rectangle(node.x+node.width,heigth-node.y-(spacing/5),spacing-(spacing/8),node.size+2*(spacing/5)))
+            elif label_shortening=="new_line":
+                if len(txt)*(label_size/2)>spacing-2*(spacing/8):
+                    txt=[txt[:int((spacing-2*(spacing/8))/(label_size/2))],txt[int((spacing-2*(spacing/8))/(label_size/2)):]]
+                    while len(txt)*l>node.size+2*(spacing/8):
+                        l-=1
+
+
+            if label_position=="start_end":
+                if node.label not in [n.label for n in points][:node.index] or node.label not in [n.label for n in points][node.index+1:] and node.index<len(points) and node.x!=max([n.x for n in points]):
+                    if label_shortening=="clip":
+                        label= draw.Text(txt,x=node.x+node.width+(spacing/8),y=heigth-node.y+(node.size/2),fontSize=l,fill=label_color,clip_path=clip)
+                    else:
+                        label= draw.Text(txt,x=node.x-(spacing/8),y=heigth-node.y+(node.size/2),fontSize=l,fill=label_color,text_anchor="end")
+
+                    
+            elif label_position=="nodes":  
+                if label_shortening=="clip":        
+                
+                    label= draw.Text(txt,x=node.x+node.width+(spacing/8),y=heigth-node.y+(node.size/2),fontSize=l,fill=label_color,clip_path=clip)
+                else:
+                    label= draw.Text(txt,x=node.x+node.width+(spacing/8),y=heigth-node.y+(node.size/2),fontSize=l,fill=label_color)
+            d.append(label)
+
+    return d
