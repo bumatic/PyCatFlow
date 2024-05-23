b64cf284ce535e226a593015fc7643a1a413324d
bumatic <marcus.burkhardt@gmail.com>
2024-05-23 18:11:37 +0200

    removed an error

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pycatflow", 
-    version="0.0.9",
+    version="0.1.0",
     author="Marcus Burkhardt",
     author_email="marcus.burkhardt@gmail.com",
     description="A tool for visualizing categorical data over time.",
