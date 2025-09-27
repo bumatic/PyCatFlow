e7476c4401480c1d3de923abcf69cc3857e3cd22
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2025-09-27 21:25:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pycatflow",
-    version="0.2.0.1",
+    version="0.2.1",
     author="Marcus Burkhardt",
     author_email="marcus.burkhardt@gmail.com",
     description="A tool for visualizing categorical data over time.",
